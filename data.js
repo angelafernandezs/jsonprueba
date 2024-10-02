068b85d98b30197b66ffd5a1e70b2e0660391c81
Angela Fernández <94569441+angelafernandezs@users.noreply.github.com>
2024-10-02 12:10:20 -0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,88 @@
+[
+    {
+        "nombre": "NUX",
+        "descripcion": "Creación de banca para una persona a partir del principio de tensoestructuras, todo está unido a partir de la tensión de las cuerdas sin ningún tipo de pegamento.",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/495d07db-df91-4258-9afc-c7e121f3ba58",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Lampara Courvé",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/3f862e1d-b1c4-469d-af71-896736c5f5f4",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Ananya",
+        "fecha": "2022",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/dd716c1c-00f4-492a-95f4-014f4ee9fdbb",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Join the Everydaymess|STUSSYXNBHD",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/6ca2555b-c387-483a-b3ba-e3c101027497",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Láminas y Mostrario",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/035fc45d-318a-497d-9ac8-cd794b44b1f1",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Editorial inspirada en Vogue",
+        "fecha": "2022",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/6dcfde02-1bee-4158-a7d1-56a0a9d3c25a",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Afiche Canción del Niño y el Ciervo",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/d78c269f-46d5-45cf-8cd9-a2566a7d9b18",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Afiche No sé que Decirte",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/7a28248c-58c8-4550-bade-5c867c47d6d2",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Sensaciones",
+        "fecha": "2023",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/ad9d522e-f592-42c0-b5aa-896350e64768",
+        "github": "https://github.com/angelafernandezs"
+    },
+    {
+        "nombre": "Cerámicas",
+        "fecha": "2024",
+        "signo": "Artesanía",
+        "foto": "https://github.com/user-attachments/assets/6deb9b34-d7db-4d37-b646-4bce67c3b636",
+        "github": "https://github.com/angelafernandezs"
+    }
+    {
+        "nombre": "Design Sketching",
+        "fecha": "2024",
+        "signo": "Industrial",
+        "foto": "https://github.com/user-attachments/assets/e8babc43-b308-4043-914b-2f1440059311",
+        "github": "https://github.com/angelafernandezs"
+    }
+    {
+        "nombre": "Afiche Cerámicas",
+        "fecha": "2024",
+        "signo": "Editorial",
+        "foto": "https://github.com/user-attachments/assets/10d2328e-570e-448d-ad20-fd9d010e8840",
+        "github": "https://github.com/angelafernandezs"
+    }
+]
+
